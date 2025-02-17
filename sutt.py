17921800ef3ec052bccd21dcdb532f0df06d4e8d
P-cloud69 <f20240724@pilani.bits-pilani.ac.in>
2025-02-17 23:49:24 +0530

    Add files via upload

---
+++ sutt.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import json
+
+df = pd.read_excel('C:\\Users\\the47\\Downloads\\Mess Menu Sample.xlsx', header=None)
+
+dates = df.iloc[1]
+dates = pd.to_datetime(dates).dt.date 
+
+messmenu = {}
+
+indices_1 = [3,4,5,6,7,8,9,10,11]
+indices_2 = [14,15,16,17,18,19,20,21]
+indices_3 = [24,25,26,27,28,29,30]
+
+
+
+for col in df.columns:
+  
+    date = str(dates[col])
+    unique_key = f"{col}_{date}" 
+    breakfast = df.loc[indices_1, col].astype(str).tolist()
+    lunch = df.loc[indices_2, col].astype(str).tolist()
+    dinner = df.loc[indices_3, col].astype(str).tolist()
+    
+    breakfast = [item if "**************" not in item else None for item in breakfast]
+    lunch = [item if "**************" not in item else None for item in lunch]
+    dinner = [item if "**************" not in item else None for item in dinner]
+    breakfast = [item for item in breakfast if item not in [None, 'nan']]
+    lunch = [item for item in lunch if item not in [None, 'nan']]
+    dinner = [item for item in dinner if item not in [None, 'nan']]
+    messmenu[date] = {
+        "BREAKFAST": breakfast,
+        "LUNCH": lunch,
+        "DINNER": dinner
+    }
+
+
+
+df_mess_menu = pd.DataFrame.from_dict(messmenu, orient="index")
+df_mess_menu = df_mess_menu.map(lambda x: None if isinstance(x, str) and "****" in x else x)
+
+df_mess_menu = df_mess_menu.dropna(how='all')
+
+df_mess_menu.to_json('messmenutask.json', orient='index', indent=4)
+jsonmenu = json.dumps(messmenu, indent=2)
+print(jsonmenu)
